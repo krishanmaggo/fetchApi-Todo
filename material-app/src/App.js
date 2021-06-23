411e635029b4020e30de660248a3b634c44b72dc
krishanmaggo <c.krishanmaggo@gmail.com>
2021-06-23 15:39:04 +0530

    To do List

--- material-app/src/App.js
+++ material-app/src/App.js
@@ -1,12 +1,84 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { Button, Card, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Todo({ todo, index, markTodo, removeTodo }) {
+  return (
+    <div className="todo">
+      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
+        {todo.text}
+      </span>
+      <div>
+        <Button variant="outline-success" onClick={() => markTodo(index)}>
+          ✓
+        </Button>{" "}
+        <Button variant="outline-danger" onClick={() => removeTodo(index)}>
+          ✕
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function FormTodo({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>
+          <b>Add Todo</b>
+        </Form.Label>
+        <Form.Control
+          type="text"
+          className="input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Add new todo"
+        />
+      </Form.Group>
+      <Button variant="primary mb-3" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
 
 function App() {
   const [query, setQuery] = useState("page=1");
   const [data, setData] = useState([]);
-  const [value, setValue] = useState('');
-  const [list, setList] = useState([]);
 
+  const [todos, setTodos] = useState([
+    {
+      text: "This is a sampLe todo",
+      isDone: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
   const page = [
     {
@@ -27,17 +99,7 @@ function App() {
         console.log(data.data[2].first_name);
       });
   }, [query]);
- 
-  useEffect(() => {
-   console.log('value',list)
-  }, [list])
 
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    setList([value])
-    console.log('list',e.target.value)
-    
-  }
   return (
     <div className="App">
       <div>
@@ -80,16 +142,27 @@ function App() {
         </table>
       </div>
       <div>
-        <form >
-          <input placeholder={'Enter Todo Item'} 
-          value={value} onChange={(e)=>{setValue(e.target.value)}}/>
-          <button onClick={(e)=>{handleSubmit(e)}}>Add Item</button>
-          
-        </form>
-        {list && list.map((item,index)=>{
-          return <h4 key={index}>{item}</h4>
-
-        })}
+        <div className="app">
+          <div className="container">
+            <h1 className="text-center mb-4">Todo List</h1>
+            <FormTodo addTodo={addTodo} />
+            <div>
+              {todos.map((todo, index) => (
+                <Card>
+                  <Card.Body>
+                    <Todo
+                      key={index}
+                      index={index}
+                      todo={todo}
+                      markTodo={markTodo}
+                      removeTodo={removeTodo}
+                    />
+                  </Card.Body>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
